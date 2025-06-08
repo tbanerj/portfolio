48642073f7ee245103b18c53246ef77215660ff9
tbanerj <trinavbanerjee7@gmail.com>
2025-06-07 18:26:35 -0700

    added https

---
+++ singing-portfolio/server.js
@@ -0,0 +1,24 @@
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'certs/server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'certs/server.cer')),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(443, err => {
+    if (err) throw err;
+    console.log('> Server running at https://localhost');
+  });
+});
